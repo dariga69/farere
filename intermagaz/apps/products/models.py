29ea1935eee216a4ef804da9911a6e9d15dc204b
dariga69 <dariga6996@gmail.com>
2023-09-04 19:04:26 +0600

    first commit

---
+++ intermagaz/apps/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
+    country = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
