4fe1119e3591018c63c60b1b6b3a5ca2ba8f0f24
dariga69 <dariga6996@gmail.com>
2023-09-13 18:51:33 +0600

    chus

--- intermagaz/intermagaz/settings.py
+++ intermagaz/intermagaz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kx=#v9t!k%3f04y7!ty%s=$!veq-$ykswt5lzdz2w365o%c&#0'
+SECRET_KEY = config('SECRET_KEY")')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
