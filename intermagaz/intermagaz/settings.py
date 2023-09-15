7a810376e3a3bdb39d0e4556dbe4840e90e77b75
dariga69 <dariga6996@gmail.com>
2023-09-15 17:47:06 +0600

    sgl

--- intermagaz/intermagaz/settings.py
+++ intermagaz/intermagaz/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = 'intermagaz.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config("DB_NAME"),
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': config("DB_HOST"),
+#         'PORT': config("DB_PORT"),
+#     }
+# }
 
 
 # Password validation
